52e8b04d8d52cf79773eeb636b143b5c2eb31016
Maria Emilia Päivärinta <eemilia.paivarinta@gmail.com>
2024-08-24 18:18:13 +0200

    Create functionality for API-request to get book covers.

--- index.js
+++ index.js
@@ -2,11 +2,14 @@ import express from "express";
 import bodyParser from "body-parser";
 import pg from "pg";
 import dotenv from "dotenv";
+import axios from "axios";
 
 dotenv.config(); // Lataa ympäristömuuttujat .env-tiedostosta
 
 const app = express();
 const port = 3000;
+const API_URL = "https://covers.openlibrary.org/b/isbn/"; 
+// Muista -S.jpg loppu apin urlissa
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
@@ -32,11 +35,43 @@ async function getBookData() {
   return data;
 }
 
-//app.get("/test", async (req, res) => {
-//  const test = await getBookData();
-//  console.log(test);
-//  res.redirect("/");
-//})
+app.get("/test", async (req, res) => {
+  const test = await getApiData();
+  //console.log(test);
+  res.redirect("/");
+});
+
+
+// Kokeillaan datan hakua APIa varten
+async function getApiData() {
+  // Hae coverphoton data databasesta
+  const result = await db.query("SELECT cover FROM books ORDER BY id ASC;");
+  const data = result.rows;
+
+  const coverUrls = [];
+
+  const apiPromises = data.map(async (row) => {
+    //console.log(row.cover);
+    // Build the apiUrl
+    const apiUrl = `${API_URL}${row.cover}-M.jpg`;
+
+    // Api query
+    const apiResponse = await axios.get(apiUrl);
+
+    // Add URL's to coverUrls array
+    coverUrls.push(apiResponse.config.url);
+  })
+
+  // Promise all helps to make the requests faster
+  await Promise.all(apiPromises);
+
+  // Testaus - tulostetaan kaikki saadut URL:t
+  //console.log(coverUrls);
+
+  return coverUrls;
+
+
+};
 
 // Reitti, joka näyttää index.ejs-tiedoston
 app.get("/", async (req, res) => {
@@ -44,9 +79,25 @@ app.get("/", async (req, res) => {
   try {
     // Haetaan funktion takaa data
     const bookData = await getBookData();
+    console.log(bookData);
+    // Haetaan apiData
+    const coverData = await getApiData();
+    console.log(coverData);
+
+
+    // Yhdistetään molempien taulukoiden vastaavat elementit
+    const combinedData = bookData.map((book, index) => {
+      return {
+        ...book,          // Lisää kaikki book-objektin kentät
+        coverUrl: coverData[index] // Lisää coverData-taulukon vastaava URL
+      };
+    });
+
+    console.log(combinedData);
+
     // Renderöidään alkunäyttö ja viedään sinne tarvittava data
     res.render("index.ejs", {
-    bookList: bookData
+    bookList: combinedData
   });
 
   } catch (error) {
@@ -75,11 +126,12 @@ app.post("/newBook", async (req, res) => {
   const newRating = req.body.rating;
   const newDate = req.body.date;
   const newContent = req.body.content;
+  const newCover = req.body.cover;
 
 
   // 2. Lisätään tiedot tietokantaan
-  const result = await db.query("INSERT INTO books (title, rating, data, content) VALUES ($1, $2, $3, $4) RETURNING *", 
-    [newTitle, newRating, newDate, newContent]
+  const result = await db.query("INSERT INTO books (title, rating, data, content, cover) VALUES ($1, $2, $3, $4, $5) RETURNING *", 
+    [newTitle, newRating, newDate, newContent, newCover]
   );
 
   console.log(result.rows);
@@ -127,10 +179,11 @@ app.post("/bookEdit/:id", async (req, res) => {
   const editRating = req.body.rating;
   const editDate = req.body.data;
   const editContent = req.body.content;
+  const editCover = req.body.cover;
 
   // Muokataan tiedot databaseen
-  await db.query("UPDATE books set title = $1, rating = $2, data = $3, content = $4 WHERE id = $5",
-    [editTitle, editRating, editDate, editContent, id]
+  await db.query("UPDATE books set title = $1, rating = $2, data = $3, content = $4, cover = $5 WHERE id = $6",
+    [editTitle, editRating, editDate, editContent, editCover, id]
   );
 
   // 3. Siirrytään takaisin etusivulle
